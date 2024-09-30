dd682e95f1f8edc72bdf0595df3c2098fbed3788
RaimonxDev <ramonmartinezcordero@gmail.com>
2024-09-30 14:52:27 -0300

    add ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component, inject, OnInit } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { HttpClient } from '@angular/common/http';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatIconModule, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, MatIconModule, RouterLink, RouterLinkActive, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
